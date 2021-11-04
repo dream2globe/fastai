7f0a1af1cfd66a4db96802a91269bd6d75a8bd90
Kang Seonghyeon <shyeon.kang@gmail.com>
2021-11-05 08:50:56 +0900

    modified contents of chapter 5

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,1 +1,3 @@
-docker run --gpus all --name fastai_jupyter --ipc=host --rm -it -p 8888:8888 -v /home/shyeon/workspace/python/fastai:/workspace fastai_notebook
+mkdir -p $HOME/.fastai
+mkdir -p $HOME/.cache
+docker run --gpus all --name fastai_jupyter --ipc=host --rm -it -p 8888:8888 -v /home/shyeon/workspace/python/fastai:/workspace -v /home/shyeon/.fastai:/root/.fastai -v /home/shyeon/.cache:/root/.cache fastai_notebook
