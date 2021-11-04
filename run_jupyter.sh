93637e36ea033ee29c166ee1acd4bd20967c27b7
Kang Seonghyeon <shyeon.kang@gmail.com>
2021-11-05 08:05:31 +0900

    added .gitignore, contents of chapter 5, run_jupyter.sh

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,2 +1,1 @@
-# docker run --name jupyter_gpu --runtime nvidia --rm --user 1000:1000 -it -v /home/shyeon/workspace/python/fastai:/home/jupyter -p 8080:8080 -e NOTEBOOK_DISABLE_ROOT=true gcr.io/kaggle-gpu-images/python 
-docker run --name fastai_jupyter_gpu --gpus all --rm -it -v $(pwd):/home/shyeon -p 8888:8888 fastai_jupyter_gpu /bin/bash
+docker run --gpus all --name fastai_jupyter --ipc=host --rm -it -p 8888:8888 -v /home/shyeon/workspace/python/fastai:/workspace fastai_notebook
